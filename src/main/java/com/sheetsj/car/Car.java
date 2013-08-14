7738a56b180056812072f49ce9aa77d0072f7817
Jeff Sheets <jeffsheets@gmail.com>
2013-08-13 23:32:59 -0500

    initial setup

---
+++ src/main/java/com/sheetsj/car/Car.java
@@ -0,0 +1,63 @@
+package com.sheetsj.car;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "car")
+public class Car implements java.io.Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private Integer year;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn
+	private Make make;
+	
+	private String model;
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public Make getMake() {
+		return make;
+	}
+
+	public void setMake(Make make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ src/main/java/com/sheetsj/car/Make.java
@@ -0,0 +1,31 @@
+package com.sheetsj.car;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "make")
+public class Make implements java.io.Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+}
