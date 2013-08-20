5507bcaf05316d7f58c6cdec9e9cbbf0c741b863
Jeff Sheets <jeffsheets@gmail.com>
2013-08-19 23:31:39 -0500

    more test samples

--- src/test/java/com/sheetsj/car/CarRepositoryIT.java
+++ src/test/java/com/sheetsj/car/CarRepositoryIT.java
@@ -1,6 +1,11 @@
 package com.sheetsj.car;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import java.util.List;
 
@@ -41,6 +46,31 @@ public class CarRepositoryIT extends IntegrationTestBaseClass {
 		assertEquals("JunitMotors", result.getMake().getName());
 		assertEquals(make.getId(), result.getMake().getId());
 		assertEquals(car.getDescription(), result.getDescription());
+	}
+	
+	/**
+	 * Now using Hamcrest for the same matchers
+	 */
+	@Test
+	public void testFindByModel_withHamcrest() {
+		Manufacturer make = new Manufacturer("JunitMotors");
+		make = manufacturerRepository.save(make);
+
+		Car car = new Car(2013, make, "JunitModel", "LT FWD 3.6L V6 DOHC 24V");
+		car = carRepository.save(car);
+		
+		List<Car> results = carRepository.findByModel("JunitModel");
+		
+		assertEquals(1, results.size());
+		
+		Car result = results.get(0);
+		assertThat(result.getYear().intValue(), is(equalTo(2013)));
+		assertThat(result.getMake().getName(), is("JunitMotors"));
+		assertThat(result.getMake().getId(), is(make.getId()));
+		assertThat(result.getDescription(), is(car.getDescription()));
+		
+		//containsString
+		//equalToIgnoringCase
 	}
 
 }
